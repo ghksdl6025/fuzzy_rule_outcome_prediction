dfd778118dcdcd42a56d56344295b23af4d54c9a
Suhwan Lee <ghksdl6025@unist.ac.kr>
2020-09-18 16:00:14 +0900

    Upload files

--- concatingohe_fuzzy.py
+++ concatingohe_fuzzy.py
@@ -9,4 +9,18 @@ for t in list(ohe_df.columns.values):
         droplist.append(t)
 ohe_df = ohe_df.drop(columns=droplist,axis=1)
 dfn = pd.merge(fuzzified_df,ohe_df,on='Case ID')
-dfn.to_csv('./concatanated_fuzzy_tx.csv',index=False)
+# dfn.to_csv('./concatanated_fuzzy_tx.csv',index=False)
+print()
+label1df = dfn[dfn['Label_1']==1]
+label0df = dfn[dfn['Label_0']==1]
+
+meaningfulcol = set()
+for col in dfn.columns.values:
+    if len([x for x in list(label1df[col]) if x != 0]) >= int(len(label1df)*0.1):
+        meaningfulcol.add(col)
+
+    if len([x for x in list(label0df[col]) if x != 0]) >= int(len(label0df)*0.1):
+        meaningfulcol.add(col)
+dfn = dfn.loc[:,list(meaningfulcol)]
+print(dfn.shape)
+dfn.to_csv('./concatanated_fuzzy_tx.csv')
